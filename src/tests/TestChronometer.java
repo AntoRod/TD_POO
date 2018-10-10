f2f114035a1a46c672f983447a2619d2e9e1abab
TsuKuSs <TsuKuSs@PC-Antony>
2018-10-10 11:43:53 +0200

    Improved TD5 - Chronometer (test)

--- src/tests/TestChronometer.java
+++ src/tests/TestChronometer.java
@@ -1,20 +1,29 @@
 package tests;
 
+import java.util.Random;
 import datas.*;
 
 public class TestChronometer {
 	public static void main(String[] args) {
 		Chronometer chronometer = new Chronometer();//Mettre des valeurs pour tester plus rapidement (ou diminuer le Thread.sleep)
 		System.out.println(chronometer);
+		
+
+		
 		while(true) {
 			try {
-				Thread.sleep(1);
-				chronometer.decrease();
-				System.out.println(chronometer);
+				Boolean test = new Random().nextBoolean();
+				Thread.sleep(1000);
+				if(test) {
+					chronometer.increase();
+					System.out.print("INCREASED: ");
+				}
+				else {
+					System.out.print("DECREASED: ");
+					chronometer.decrease();
+				}
+				System.out.print(chronometer+"\n");
 			} catch (InterruptedException e) {e.printStackTrace();}
-
 		}
-		
 	}
-
 }
